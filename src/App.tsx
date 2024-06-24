1b3df9340c5e13ed0c7773f1fe3dff18174f161b
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-06-24 16:38:06 +0100

    work in progress

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import Header from './components/Header.tsx';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,4 @@
+import './Header.css';
+export default function Header() {
+    return <h1 className="title rainbow-effect">Game of Life</h1>
+}
