c3e72550f482961855dec3b9b7560cfc5538d23b
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-07-19 23:53:11 +0100

    working on checking neighbour cells

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,16 +1,27 @@
 import { describe, test, expect } from "vitest";
-import { getNextGen } from './utils';
+import { getNextGen } from "./utils";
 
 describe("getNextGen", () => {
   test("should throw error when called with no cells", () => {
-    expect(()=> getNextGen([[]]) ).toThrowError();
+    expect(() => getNextGen([[]])).toThrowError();
   });
 
-  test("should return single dead cell when called with single dead cell", ()=> {
+  test("should return single dead cell when called with single dead cell", () => {
     expect(getNextGen([[false]])).toEqual([[false]]);
-  })
+  });
 
-  test("should return a single dead cell when called with single alive cell", ()=> {
+  test("should return a single dead cell when called with single alive cell", () => {
     expect(getNextGen([[true]])).toEqual([[false]]);
-  })
+  });
+  test("should stay alive when cell is alive and has 3 neighbours", () => {
+    const board = [
+      [true, true],
+      [true, true],
+    ];
+    const result = getNextGen(board);
+    expect(result).toEqual([
+      [true, true],
+      [true, true],
+    ]);
+  });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,110 @@
-
 export function getNextGen(board: boolean[][]): boolean[][] {
-    const clonedBoard = structuredClone(board);
-    if(board[0].length === 0) {
-        throw new Error('Board rows must have cells');
+  if (board[0].length === 0) {
+    throw new Error("Board rows must have cells");
+  }
+
+  const clonedBoard = structuredClone(board);
+
+  const rowCount = board.length;
+  const colCount = board[0].length;
+
+  for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+    const row = board[rowIndex];
+    for (let colIndex = 0; colIndex < row.length; colIndex++) {
+      // we have our rowIndex and our cellIndex. We have a specific cell.
+      // count neighbours
+      let neighbourCount = countNeighbours(
+        rowIndex,
+        rowCount,
+        colIndex,
+        colCount,
+        board
+      );
+
+      // top-left neighbour
+      if (
+        rowIndex !== 0 &&
+        colIndex !== 0 &&
+        board[rowIndex - 1][colIndex - 1]
+      ) {
+        neighbourCount++;
+      }
+
+      // top neighbour
+      if (rowIndex !== 0 && board[rowIndex - 1][colIndex]) {
+        neighbourCount++;
+      }
+
+      // top right neighbour
+      if (
+        rowIndex !== 0 &&
+        colIndex !== row.length - 1 &&
+        board[rowIndex - 1][colIndex + 1]
+      ) {
+        neighbourCount++;
+      }
+
+      //middle left neighbour
+      if (colIndex !== 0 && board[rowIndex][colIndex - 1]) {
+        neighbourCount++;
+      }
+
+      //middle right neighbour
+      if (colIndex !== row.length - 1 && board[rowIndex][colIndex + 1]) {
+        neighbourCount++;
+      }
+
+      //bottom left neighbour
+      if (
+        rowIndex !== board.length - 1 &&
+        colIndex !== 0 &&
+        board[rowIndex + 1][colIndex - 1]
+      ) {
+        neighbourCount++;
+      }
+
+      //bottom neightbour
+      if (rowIndex !== board.length - 1 && board[rowIndex - 1][colIndex - 1]) {
+        neighbourCount++;
+      }
+
+      //bottom right neighbour
+      if (
+        rowIndex !== board.length - 1 &&
+        colIndex !== row.length - 1 &&
+        board[rowIndex + 1][colIndex + 1]
+      ) {
+        neighbourCount++;
+      }
     }
-    return clonedBoard;
+    if (row.includes(true)) {
+      return [[false]];
+    }
+  }
+  return clonedBoard;
+}
+function countNeighbours(
+  rowIndex: number,
+  rowCount: number,
+  colIndex: number,
+  colCount: number,
+  board: boolean[][]
+) {
+  let neighbourCount = 0;
+
+  for (let rowDiff = -1; rowDiff <= 1; rowDiff++) {
+    for (let colDiff = -1; colDiff <= 1; colDiff++) {
+      if (
+        !(rowDiff === 0 && colDiff === 0) &&
+        0 <= rowIndex + rowDiff &&
+        rowIndex + rowDiff < rowCount &&
+        0 <= colIndex + colDiff &&
+        colIndex + colDiff < colCount &&
+        board[rowIndex + rowDiff][colIndex + colDiff]
+      ) {
+        neighbourCount++;
+      }
+    }
+  }
+  return neighbourCount;
 }
